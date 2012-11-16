a0bdf121c0103244a5946fc910c0c30786e3b741
Tom Yeh <doubleshow@gmail.com>
2012-11-16 11:28:00 -0700

    saveAsImage implemented for label and box elements

--- src/main/java/org/sikuli/api/visual/DrawingCanvas.java
+++ src/main/java/org/sikuli/api/visual/DrawingCanvas.java
@@ -1,10 +1,16 @@
 package org.sikuli.api.visual;
 
+import java.awt.BasicStroke;
 import java.awt.Color;
 import java.awt.Rectangle;
 import java.util.List;
 
 import com.google.common.collect.Lists;
+
+import edu.umd.cs.piccolo.PNode;
+import edu.umd.cs.piccolo.nodes.PPath;
+import edu.umd.cs.piccolo.nodes.PText;
+import edu.umd.cs.piccolox.nodes.PShadow;
 
 
 class Element {
@@ -14,8 +20,12 @@ class Element {
 	public int height;	
 
 	public Color lineColor = Color.red;
-	public int lineThickness = 5;
-
+	public Color color = Color.black;
+	public int lineWidth = 2;
+	
+	public PNode createPNode(){
+		return new PNode();
+	}
 }
 
 class BoxElement extends Element {
@@ -25,14 +35,91 @@ class LabelElement extends Element {
 	public String text;
 }
 
+
+class PNodeFactory {
+	
+	static public PNode createFrom(Element element){
+		Class<? extends Element> clazz = element.getClass();
+		if (clazz == LabelElement.class){
+			return createFrom((LabelElement)element);
+		}else if (clazz == BoxElement.class){
+			return createFrom((BoxElement) element);
+		}	
+		return new PNode();
+	}
+	
+	static public PNode createFrom(LabelElement element){
+		final PText txt = new PText(element.text);
+		txt.setTextPaint(Color.black);
+		txt.setPaint(Color.yellow);
+		txt.setTextPaint(element.color);
+		
+		PNode labelNode = new PNode();
+		labelNode.setPaint(Color.yellow);
+		labelNode.addChild(txt);
+		labelNode.setHeight(txt.getHeight()+2);
+		labelNode.setWidth(txt.getWidth()+4);
+		txt.setOffset(2,1);
+	
+		
+		labelNode.setOffset(element.x, element.y);
+		return addShadow(labelNode);
+	}
+	
+	static public PNode createFrom(BoxElement element){
+		PPath p = PPath.createRectangle(1,1,element.width,element.height);
+		p.setStrokePaint(element.lineColor);
+		p.setPaint(null);		
+		p.setStroke(new BasicStroke(element.lineWidth));
+		
+
+		PNode foregroundNode = new PNode();
+		foregroundNode.addChild(p);
+		foregroundNode.setHeight(p.getHeight()+4);
+		foregroundNode.setWidth(p.getWidth()+4);
+		p.setOffset(2,2);
+		
+		foregroundNode.setOffset(element.x, element.y);
+		
+		return addShadow(foregroundNode);
+	}
+	
+	static public void setStyle(){
+		
+	}
+	
+	static private final Color SHADOW_PAINT = new Color(10, 10, 10, 200);
+	static private PNode addShadow(PNode contentNode){
+		
+		PNode contentNodeWithShadow = new PNode();
+		
+		double xoffset = contentNode.getXOffset();
+		double yoffset = contentNode.getYOffset();
+		
+		int blurRadius = 4;
+		int tx = 5;
+		int ty = 5;
+
+		PShadow shadowNode = new PShadow(contentNode.toImage(), SHADOW_PAINT, blurRadius );		
+		contentNode.setOffset(tx, ty);
+		shadowNode.setOffset(tx - (2 * blurRadius) + 1.0d, ty - (2 * blurRadius) + 1.0d);
+
+		contentNodeWithShadow.addChild(shadowNode);
+		contentNodeWithShadow.addChild(contentNode);		      
+		contentNodeWithShadow.setOffset(xoffset - tx  - blurRadius, yoffset - ty - blurRadius);
+        return contentNodeWithShadow;
+	}
+}
+
+
 public class DrawingCanvas {
 
 	protected final List<Element> elements = 	Lists.newArrayList();;
 
-	static public class PlacementBuilder{
-
+	static public class BoxPlacementBuilder{
+		
 		private Element element;
-		public PlacementBuilder(Element newElement) {
+		public BoxPlacementBuilder(Element newElement) {
 			this.element = newElement;
 		}
 
@@ -43,6 +130,16 @@ public class DrawingCanvas {
 			element.height = r.height;			
 			return new StyleBuilder(element);
 		}
+
+	}
+	
+	static public class PlacementBuilder{
+
+		private Element element;
+		public PlacementBuilder(Element newElement) {
+			this.element = newElement;
+		}
+
 
 		public StyleBuilder above(Rectangle r){
 			return null;
@@ -73,24 +170,27 @@ public class DrawingCanvas {
 			return this;
 		}
 
-		public StyleBuilder withLineThickness(int thickness){
-			element.lineThickness = thickness;
+		public StyleBuilder withColor(Color color){
+			element.color = color;
+			return this;
+		}
+		
+		public StyleBuilder withLineWidth(int width){
+			element.lineWidth = width;
 			return this;
 		}
 
 	}
 
-	public PlacementBuilder addBox(){
-		return addElement(new BoxElement());
+	public BoxPlacementBuilder addBox(){
+		BoxElement newElement = new BoxElement();
+		elements.add(newElement);
+		return new BoxPlacementBuilder(newElement);		
 	}
 
 	public PlacementBuilder addLabel(String labelText){
 		LabelElement newElement = new LabelElement();
 		newElement.text = labelText;
-		return addElement(newElement);
-	}
-
-	private PlacementBuilder addElement(Element newElement){
 		elements.add(newElement);
 		return new PlacementBuilder(newElement);		
 	}
@@ -98,6 +198,6 @@ public class DrawingCanvas {
 	public void clear() {
 		elements.clear();	
 	}
-	
+
 
 }
--- src/main/java/org/sikuli/api/visual/ScreenRegionCanvas.java
+++ src/main/java/org/sikuli/api/visual/ScreenRegionCanvas.java
@@ -1,10 +1,18 @@
 package org.sikuli.api.visual;
 
+import java.awt.Point;
 import java.awt.Rectangle;
+import java.awt.geom.Point2D;
 import java.awt.image.BufferedImage;
 
 import org.sikuli.api.ScreenRegion;
 import org.sikuli.api.robot.desktop.DesktopScreen;
+import org.sikuli.core.cv.VisionUtils;
+
+import edu.umd.cs.piccolo.PCanvas;
+import edu.umd.cs.piccolo.PLayer;
+import edu.umd.cs.piccolo.PNode;
+import edu.umd.cs.piccolo.nodes.PImage;
 
 public class ScreenRegionCanvas extends ScreenDisplayableDrawingCanvas {
 	
@@ -31,5 +39,49 @@ public class ScreenRegionCanvas extends ScreenDisplayableDrawingCanvas {
 	
 	protected BufferedImage getBackgroundImage(){
 		return canvasScreenRegion.capture();
+	}
+	
+	public BufferedImage createImage(){
+		final PCanvas canvas = new PCanvas();
+		
+		BufferedImage backgroundImage = getBackgroundImage();
+		final PImage background = new PImage(backgroundImage);
+		canvas.getLayer().addChild(background);
+		canvas.setBounds(0,0,backgroundImage.getWidth(),backgroundImage.getHeight());
+		
+		//canvas.getL
+		PLayer layer = canvas.getLayer();
+		Rectangle r = canvasScreenRegion.getBounds();
+		System.out.println(canvasScreenRegion);
+		PLayer foregroundLayer = new PLayer();		
+		layer.addChild(foregroundLayer);		
+		foregroundLayer.setGlobalTranslation(new Point(-r.x,-r.y));
+		
+		layer.addChild(foregroundLayer);
+		
+		//layer.setOffset(new Point(-r.x,-r.y));
+		for (Element element : elements){
+			
+			//PNode node = element.createPNode();
+			PNode node = PNodeFactory.createFrom(element);
+//			node.setX(element.x - r.x);
+//			node.setY(element.y - r.y);
+//			System.out.println(element.x);
+//			System.out.println(element.y);
+//			System.out.println(r.y);
+			//node.setOffset(element.x, element.y);
+			
+			//node.setOffset(element.y - r.y);
+			//node.setGlobalTranslation(new Point(r.x,r.y));
+			//node.setO
+			//node.setOffset(new Point(-r.x,-r.y));
+			//node.setOffset(new Point(r.x,r.y));
+			foregroundLayer.addChild(node);
+		}
+		//layer.to
+		
+		//canvas.add(layer);
+		
+		return VisionUtils.createComponentImage(canvas);		
 	}
 }
