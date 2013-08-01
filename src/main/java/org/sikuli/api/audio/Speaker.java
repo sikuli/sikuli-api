81e2ada27b939c59a5ca47b3b6e4f6884502d5a3
Khalid Alharbi <Khalid3ahmed@gmail.com>
2013-08-01 00:55:31 -0600

    add more javadoc comments

--- src/main/java/org/sikuli/api/audio/Speaker.java
+++ src/main/java/org/sikuli/api/audio/Speaker.java
@@ -1,7 +1,10 @@
 package org.sikuli.api.audio;
 
 import java.net.URL;
-
+/**
+ * The Speaker interface provides definitions for the objects that represent a Speaker.
+ *
+ */
 public interface Speaker {
 	public void play(URL url);
 }
