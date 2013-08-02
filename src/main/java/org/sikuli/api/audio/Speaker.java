fd01c3d047ffc27f248c2e4b55d7c40cc2b9355f
Khalid Alharbi <Khalid3ahmed@gmail.com>
2013-08-02 11:07:31 -0600

    add javadoc comments to the audio package

--- src/main/java/org/sikuli/api/audio/Speaker.java
+++ src/main/java/org/sikuli/api/audio/Speaker.java
@@ -2,9 +2,16 @@ package org.sikuli.api.audio;
 
 import java.net.URL;
 /**
- * The Speaker interface provides definitions for the objects that represent a Speaker.
+ * The Speaker interface provides definitions for the objects that represent a Speaker that plays
+ * audio clips.
  *
  */
 public interface Speaker {
+	/**
+	 * Plays audio clip (in .wav format) at the specified URL.
+	 * This is a blocking method that plays the audio clip specified by the URL argument.
+	 * 
+	 * @param url an absolute URL giving the location of the audio clip.
+	 */
 	public void play(URL url);
 }
